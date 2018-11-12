48c87ecb2ea05ea6085470e7cfb480c49082ff12
Timwi <github@timwi.de>
2018-11-12 18:41:36 +0100

    First playable version.

--- Assets/OneHundredAndOneDalmatiansModule.cs
+++ Assets/OneHundredAndOneDalmatiansModule.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using KModkit;
+﻿using System.Collections;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
 
@@ -14,12 +11,140 @@ public class OneHundredAndOneDalmatiansModule : MonoBehaviour
     public KMBombInfo Bomb;
     public KMBombModule Module;
     public KMAudio Audio;
+    public KMRuleSeedable RuleSeedable;
+
+    public Texture[] Furspots;
+    public GameObject[] Dalmatians;
+    public GameObject DalmatianParent;
+    public MeshRenderer Fur;
+    public KMSelectable LeftArrow, RightArrow, Submit;
+    public TextMesh[] NameDisplays;
 
     private static int _moduleIdCounter = 1;
     private int _moduleId;
+    private int _solution;
+    private int _curDalmatianIndex;
+    private Coroutine _longPress;
+    private bool _solved;
+
+    private static readonly string[] _dalmatians = new[] { "Blackear", "Blackie", "Blob", "Blot", "Bon-Bon", "Bravo", "Brownie", "Bulgey", "Bump", "Cadpig", "Corky", "D.J.", "Da Vinci", "Dante", "Dash", "Dawkins", "Deja Vu", "Dingo", "Dipper", "Dipstick", "Disco", "Disel", "Dolly", "Dorothy", "Dot", "Duke", "Dylan", "Fatty", "Fidget", "Flapper", "Football", "Freckles", "Furrball", "Guy", "Growly", "Ham", "Harvey", "Holly", "Hoofer", "Hoover", "Hungry", "Inky", "Jewel", "Jolly", "Kirby", "Latch", "Lenny", "Leno", "Lipdip", "Lucky", "Ludo", "Lugnut", "Lumpy", "Missy", "Nosey", "Pandy", "Patches", "Penny", "Pepper", "Perdita", "Pickle", "Plato", "Playdoh", "Pointy", "Pokey", "Polly", "Pongo", "Pooh", "Puddles", "Purdy", "Queeny", "Roger", "Roly Poly", "Rover", "Sa-Sa", "Salter", "Scooter", "Scottie", "Sleepy", "Smokey", "Sniff", "Spanky", "Spark", "Spatter", "Speedy", "Sport", "Spot", "Spotty", "Steve", "Sugar", "Swifty", "Thunder", "Tiger", "Tiresome", "Tripod", "Two-Tone", "Wags", "Whitey", "Whizzer", "Yank", "Yoyo" };
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
+        _solved = false;
+
+        var rnd = RuleSeedable.GetRNG();
+        var skip = rnd.Next(0, 100);
+        for (var i = 0; i < skip; i++)
+            rnd.NextDouble();
+        rnd.ShuffleFisherYates(Furspots);
+        _solution = Rnd.Range(0, 101);
+        Fur.material.mainTexture = Furspots[_solution];
+        Fur.transform.localEulerAngles = new Vector3(0, Rnd.Range(0, 360f), 0);
+        _curDalmatianIndex = Rnd.Range(0, 101);
+        UpdateName();
+
+        Debug.LogFormat(@"[101 Dalmatians #{0}] Name initially shown: {1}", _moduleId, _dalmatians[_curDalmatianIndex]);
+        Debug.LogFormat(@"[101 Dalmatians #{0}] Solution: {1}", _moduleId, _dalmatians[_solution]);
+
+        LeftArrow.OnInteract = delegate { _curDalmatianIndex = (_curDalmatianIndex + 100) % 101; UpdateName(); startLongPress(100); return false; };
+        RightArrow.OnInteract = delegate { _curDalmatianIndex = (_curDalmatianIndex + 1) % 101; UpdateName(); startLongPress(1); return false; };
+
+        LeftArrow.OnInteractEnded = RightArrow.OnInteractEnded = delegate
+        {
+            if (_longPress != null)
+                StopCoroutine(_longPress);
+        };
+
+        Submit.OnInteract = delegate
+        {
+            if (_solved)
+                return false;
+            if (_curDalmatianIndex == _solution)
+            {
+                Debug.LogFormat(@"[101 Dalmatians #{0}] You submitted {1}. Correct.", _moduleId, _dalmatians[_curDalmatianIndex]);
+                Module.HandlePass();
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.CorrectChime, transform);
+                LeftArrow.gameObject.SetActive(false);
+                RightArrow.gameObject.SetActive(false);
+                StartCoroutine(solveAnimationPart1());
+                StartCoroutine(solveAnimationPart2());
+                _solved = true;
+            }
+            else
+            {
+                Debug.LogFormat(@"[101 Dalmatians #{0}] You submitted {1}. Strike.", _moduleId, _dalmatians[_curDalmatianIndex]);
+                Module.HandleStrike();
+            }
+            return false;
+        };
+    }
+
+    private static float easeInOutQuad(float t, float start, float end, float duration)
+    {
+        var change = end - start;
+        t /= duration / 2;
+        if (t < 1)
+            return change / 2 * t * t + start;
+        t--;
+        return -change / 2 * (t * (t - 2) - 1) + start;
+    }
+
+    private IEnumerator solveAnimationPart1()
+    {
+        yield return new WaitForSeconds(.3f);
+        var duration = .6f;
+        var elapsed = 0f;
+        while (elapsed < duration)
+        {
+            Submit.transform.localPosition = new Vector3(easeInOutQuad(elapsed, 0, -0.026f, duration), 0.01501f, .03f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        Submit.transform.localPosition = new Vector3(-0.026f, 0.01501f, .03f);
+    }
+
+    private IEnumerator solveAnimationPart2()
+    {
+        yield return new WaitForSeconds(.75f);
+        var ix = Rnd.Range(0, Dalmatians.Length);
+        Dalmatians[ix].SetActive(true);
+        var scale = DalmatianParent.transform.localScale;
+        var duration = .3f;
+        var elapsed = 0f;
+        while (elapsed < duration)
+        {
+            var x = elapsed / duration / 1.5f;
+            DalmatianParent.transform.localScale = new Vector3(scale.x, scale.y, 4.5f * x * (1 - x));
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        DalmatianParent.transform.localScale = scale;
+    }
+
+    private void startLongPress(int dir)
+    {
+        if (_longPress != null)
+            StopCoroutine(_longPress);
+
+        _longPress = StartCoroutine(longPress(dir));
+    }
+
+    private IEnumerator longPress(int dir)
+    {
+        yield return new WaitForSeconds(.37f);
+        while (true)
+        {
+            _curDalmatianIndex = (_curDalmatianIndex + dir) % 101;
+            UpdateName();
+            yield return new WaitForSeconds(.02f);
+        }
+    }
+
+    private void UpdateName()
+    {
+        foreach (var disp in NameDisplays)
+            disp.text = _dalmatians[_curDalmatianIndex];
     }
 }
